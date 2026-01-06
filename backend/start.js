c47efdc0e343f6248cc548753340742ffcb2008a
vk <vishnukarthik2912@gmail.com>
2026-01-06 17:54:45 +0530

    updated the backend

--- backend/start.js
+++ backend/start.js
@@ -18,7 +18,7 @@ import calculationRoutes from './routes/calculationRoutes.js'
 const app = express()
 
 // Middleware
-app.use(cors())
+app.use(cors({ origin: '*' }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
